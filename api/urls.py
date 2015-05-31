4084f1328d45b27978d414725348d246bf50f05b
ap-Codkelden <mavladi@gmail.com>
2015-05-31 17:32:58 +0300

    url fix

--- api/urls.py
+++ api/urls.py
@@ -66,7 +66,7 @@ urls = (
 
     (r'^%s/user/(?P<login>[-a-zA-Z0-9]+)/?$' % prefix, users.info),
     # user info via id
-    (r'^%s/user/id/(?P<id>[0-9]+)/?$' % prefix, users.user_info_byid),
+    (r'^%s/user/?$' % prefix, users.user_info_byid),
     # user info via settings.domain/api/me
     (r'^%s/me/?$' % prefix, users.my_info),
 
